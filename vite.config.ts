d65a4a3309b23dca6f64d947642eb264cece2ed1
Ajay Kumar Dhurwe <ajaykumardhurwe049@gmail.com>
2025-06-15 11:06:14 +0530

    deploy things are added

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/movies/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
